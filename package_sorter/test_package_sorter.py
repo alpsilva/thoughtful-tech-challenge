bf7445b82347a78c74807b9d70b1ff6ca967f59a
alpsilva <alpsilva.dev@gmail.com>
2025-11-11 18:37:16 -0300

    [tests] sort success tests

--- package_sorter/test_package_sorter.py
+++ package_sorter/test_package_sorter.py
@@ -1,7 +1,26 @@
 
 import pytest
 
-from sorter import sort, PackageSorterWrongTypeError, PackageSorterValueError
+from sorter import sort, PackageStacks, PackageSorterWrongTypeError, PackageSorterValueError
+
+@pytest.mark.parametrize("width, height, length, mass, expected_stack", [
+    (1, 2, 3, 4, PackageStacks.STANDARD.value),
+    (1.5, 2.5, 3.5, 4.5, PackageStacks.STANDARD.value),
+    (1.5, 2.5, 3, 4, PackageStacks.STANDARD.value),
+    (1, 2, 3, 20, PackageStacks.SPECIAL.value),
+    (1, 2, 3, 25, PackageStacks.SPECIAL.value),
+    (200, 10, 10, 15, PackageStacks.SPECIAL.value),
+    (10, 200, 10, 15, PackageStacks.SPECIAL.value),
+    (10, 10, 200, 15, PackageStacks.SPECIAL.value),
+    (1000, 1000, 1000, 15, PackageStacks.SPECIAL.value),
+    (200, 10, 10, 30, PackageStacks.REJECTED.value),
+    (10, 200, 10, 30, PackageStacks.REJECTED.value),
+    (10, 10, 200, 30, PackageStacks.REJECTED.value),
+    (1000, 1000, 1000, 30, PackageStacks.REJECTED.value),
+])
+def test_sort(width, height, length, mass, expected_stack):
+    stack = sort(width, height, length, mass)
+    assert stack == expected_stack
 
 @pytest.mark.parametrize("width, height, length, mass, expected_error", [
     ("1", 2, 3, 4.5, "Parameter width should be one of: int, float. Received <class 'str'>"),
