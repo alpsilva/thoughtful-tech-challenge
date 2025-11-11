3ae94afb371df5d0b0a5c32623332096ab0f47d6
alpsilva <alpsilva.dev@gmail.com>
2025-11-11 17:47:37 -0300

    [refactor] format

--- package_sorter/sort.py
+++ package_sorter/sort.py
@@ -1,32 +1,42 @@
 from typing import Union
 from enum import Enum
 
+
 class PackageStacks(Enum):
     STANDARD = "STANDARD"
     SPECIAL = "SPECIAL"
     REJECTED = "REJECTED"
 
+
 def _is_bulky(
-        width_in_cm: Union[int, float],
-        height_in_cm: Union[int, float],
-        length_in_cm: Union[int, float]) -> bool:
+    width_in_cm: Union[int, float],
+    height_in_cm: Union[int, float],
+    length_in_cm: Union[int, float],
+) -> bool:
     """
-    A package is bulky if its volume (Width x Height x Length) is greater than or equal to 1,000,000 cm³ 
+    A package is bulky if its volume (Width x Height x Length) is greater than or equal to 1,000,000 cm³
     or when one of its dimensions is greater or equal to 150 cm
     """
-    
+
     for dimension in [width_in_cm, height_in_cm, length_in_cm]:
         if dimension >= 150:
             return True
-    
+
     volume = width_in_cm * height_in_cm * length_in_cm
     return volume >= 1_000_000
 
+
 def _is_heavy(weight_in_kg: Union[int, float]) -> bool:
-    """ A package is heavy when its mass is greater or equal to 20 kg """
+    """A package is heavy when its mass is greater or equal to 20 kg"""
     return weight_in_kg >= 20
 
-def sort(width: Union[int, float], height: Union[int, float], length: Union[int, float], mass: Union[int, float]) -> str:
+
+def sort(
+    width: Union[int, float],
+    height: Union[int, float],
+    length: Union[int, float],
+    mass: Union[int, float],
+) -> str:
     """
     Receives package data and determine which stack it should to go to:
 
@@ -41,9 +51,8 @@ def sort(width: Union[int, float], height: Union[int, float], length: Union[int,
 
     if is_bulky and is_heavy:
         return PackageStacks.REJECTED.value
-    
+
     if is_bulky and is_heavy:
         return PackageStacks.SPECIAL.value
-    
+
     return PackageStacks.STANDARD.value
-    
