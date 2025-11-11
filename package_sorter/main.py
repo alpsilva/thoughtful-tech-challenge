65831def296f937be9e37c4454222f752a8f7937
alpsilva <alpsilva.dev@gmail.com>
2025-11-11 18:59:43 -0300

    [refactor] Improved main output

--- package_sorter/main.py
+++ package_sorter/main.py
@@ -2,8 +2,8 @@ from sorter import sort
 
 
 def main():
-    stack = sort(10, 20, 30, 3)
-    print(stack)
+    stack = sort(width=10, height=20, length=30, mass=3)
+    print(f"Stack for the package: {stack}")
 
 
 if __name__ == "__main__":
